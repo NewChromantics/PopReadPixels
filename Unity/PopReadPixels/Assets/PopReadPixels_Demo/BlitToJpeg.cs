1a05ccb5ce3e652c745c852836488981ef369959
Graham Reeves <graham@grahamreeves.com>
2017-06-04 20:08:39 +0100

    Demo writes out jpeg

--- Unity/PopReadPixels/Assets/PopReadPixels_Demo/BlitToJpeg.cs
+++ Unity/PopReadPixels/Assets/PopReadPixels_Demo/BlitToJpeg.cs
@@ -11,16 +11,36 @@ public class BlitToJpeg : MonoBehaviour {
 
 	public RenderTexture	DynamicTexture;
 	public Material			DynamicShader;
+	public string			OutputFilename = "Test.jpg";
 	public UnityEvent_Bytes	OnJpegEncoded;
 
 	void Update () {
 
+
 		System.Action<byte[],Vector2,int,string> OnTexturePixels = (Pixels, Size, Channels,Error) => {
-			Debug.Log("Read pixels: " + Error);
+
+			if ( Error != null )
+				Debug.Log("Read pixels: " + Error);
+
+			if ( Pixels != null )
+			{
+				try
+				{
+					var Jpeg = PopEncodeJpeg.EncodeToJpeg( Pixels, (int)Size.x, (int)Size.y, Channels, true ); 
+					OnJpegEncoded.Invoke(Jpeg);
+					System.IO.File.WriteAllBytes( OutputFilename, Jpeg );
+				}
+				catch(System.Exception e)
+				{
+					Debug.LogException( e, this );
+				}
+			}
 		};
 
 		Graphics.Blit (null, DynamicTexture, DynamicShader);
 		PopReadPixels.ReadPixelsAsync (DynamicTexture, OnTexturePixels);
+
+		PopReadPixels.FlushDebug ();
 
 	}
 }
