1ed992490b55a1f5fb2f8d4f01fe4ed31c2b2879
Graham Reeves <graham@grahamreeves.com>
2017-06-08 13:18:10 +0100

    added new filepath attrib to the jpeg->file, and incrementing-filename output option

--- Unity/PopReadPixels/Assets/PopReadPixels_Demo/JpegToFile.cs
+++ Unity/PopReadPixels/Assets/PopReadPixels_Demo/JpegToFile.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class JpegToFile : MonoBehaviour {
 
+	public bool			IncrementFilename = false;
+	int FilenameCount = 0;
+
+	[FilePath("jpg",order=0)]
+	//[ShowIf("IsFixedFilename",order=1)]
 	public string		OutputFilename = "Test.jpg";
 
+	[FilePath(FilePathAttribute.PathType.Folder,order=0)]
+	//[ShowIf("IsIncrementingFilename",order=1)]
+	public string		OutputFileDir = "";
+
+	bool IsFixedFilename()
+	{
+		return !IncrementFilename;
+	}
+	bool IsIncrementingFilename()
+	{
+		return IncrementFilename;
+	}
 
 	public void			OnPixels(byte[] Pixels,Vector2 Size,int Channels)
 	{
+		var NextFilename = OutputFilename;
+
+		if (IncrementFilename) {
+			NextFilename = OutputFileDir + System.IO.Path.DirectorySeparatorChar + "Frame" + FilenameCount + ".jpg";
+			FilenameCount++;
+		}
+
 		//	async
 		System.Threading.ThreadPool.QueueUserWorkItem ((WorkItem) => {
 			var Jpeg = PopEncodeJpeg.EncodeToJpeg( Pixels, (int)Size.x, (int)Size.y, Channels, true ); 
-			System.IO.File.WriteAllBytes( OutputFilename, Jpeg );
+			System.IO.File.WriteAllBytes( NextFilename, Jpeg );
 		}
 		);
 
