d2f1d3473539278f427705e12ca0f99cb39ac58c
Graham Reeves <graham@grahamreeves.com>
2017-07-21 00:08:54 +0100

    Testing other unty formats. Now showing actual texture we read from & first pixel byte values

--- Unity/PopReadPixels/Assets/PopReadPixels_Demo/SetRawImageColour.cs
+++ Unity/PopReadPixels/Assets/PopReadPixels_Demo/SetRawImageColour.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 public class SetRawImageColour : MonoBehaviour {
 
 	public RawImage		Target;
+	public Text			TargetString;
 	public Color32		ErrorColour = Color.red;
 
 	void SetColour(Color32 Colour)
+	{
+		Target.color = Colour;
+	}
+
+	void SetColour(Color Colour)
 	{
 		Target.color = Colour;
 	}
@@ -25,6 +31,31 @@ public class SetRawImageColour : MonoBehaviour {
 		{
 			var Colour = new Color32( Bytes[0], Bytes[1], Bytes[2], 255 );
 			SetColour( Colour);
+
+			if ( TargetString != null )
+			{
+				TargetString.text = "Byte[0] = ( " + Bytes[0] + " " + Bytes[1] + " " + Bytes[2] + " " + Bytes[3] + " )";
+			}
+		}
+		catch
+		{
+			SetColour( ErrorColour );
+		}
+	}
+
+
+
+	public void OnPixels(float[] Bytes,Vector2 Size,int ChannelCount)
+	{
+		try
+		{
+			var Colour = new Color( Bytes[0], Bytes[1], Bytes[2], 255 );
+			SetColour( Colour);
+
+			if ( TargetString != null )
+			{
+				TargetString.text = "Byte[0] = ( " + Bytes[0] + " " + Bytes[1] + " " + Bytes[2] + " " + Bytes[3] + " )";
+			}
 		}
 		catch
 		{
