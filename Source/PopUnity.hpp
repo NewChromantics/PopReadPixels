b24f8f92395515c4db08dee8477e942d19baddfe
Graham Reeves <graham@grahamreeves.com>
2017-07-21 01:31:46 +0100

    android/ios compile fix

--- Source/PopUnity.hpp
+++ Source/PopUnity.hpp
@@ -9,7 +9,7 @@
 #if defined(TARGET_WINDOWS)
 #define __api(returntype)	returntype __stdcall 
 #define __export			extern "C" __declspec(dllexport)
-#elif defined(TARGET_OSX)
+#elif defined(TARGET_OSX) || defined(TARGET_IOS)|| defined(TARGET_ANDROID)
 #define __api(returntype)	extern "C" returntype
 #define __export			extern "C"
 #endif
